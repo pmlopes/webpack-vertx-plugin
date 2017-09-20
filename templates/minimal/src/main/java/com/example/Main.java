8e7e5a0fc9a6aa6c15eccda0640792347b50cd89
Paulo Lopes <paulo@mlopes.net>
2017-09-20 13:57:53 +0200

    test new faster hot reload

--- templates/minimal/src/main/java/com/example/Main.java
+++ templates/minimal/src/main/java/com/example/Main.java
@@ -8,14 +8,18 @@ import xyz.jetdrone.vertx.hot.reload.HotReload;
 
 public class Main extends AbstractVerticle {
 
+  private static final boolean DEBUG = true;
+
   @Override
   public void start() throws Exception {
 
     final Router router = Router.router(vertx);
-    // development hot reload
-    router.get().handler(HotReload.create());
+    if (DEBUG) {
+      // development hot reload
+      router.get().handler(HotReload.create());
+    }
     // Serve the static resources
-    router.route().handler(StaticHandler.create());
+    router.route().handler(DEBUG ? HotReload.createStaticHandler() : StaticHandler.create());
 
     vertx.createHttpServer().requestHandler(router::accept).listen(8080);
   }
